4fc2b4a1c27d825ff306deb9799968bf4316136b
Sam <daisiran9909@126.com>
2019-08-24 12:23:33 +0800

    finish curve optimize(not checked)

--- Turtle/Turtle.h
+++ Turtle/Turtle.h
@@ -18,7 +18,7 @@ typedef enum {
 }Gesture;
 
 typedef enum {
-	ONLINE, ONCURVE_BEGIN, ONCURVE_MIDDLE
+	ONLINE, ONCURVE_BEGIN, ONCURVE_MIDDLE, ONCURVE_END;
 }Place;
 
 struct RoboTurtle {
@@ -33,23 +33,89 @@ struct RoboTurtle {
 
 void RoboTurtle::takeAction() {
 	switch (status) {
-		case STAY: {
-			stay_Middle();
-			//sleep(sleeptime);
-			cout<<"STAYING"<<endl;
-			status = LINE_DETECT;
+	case STAY: {
+		stay_Middle();
+		//sleep(sleeptime);
+		switch (place) {
+		case ONLINE: {
+			walkStep += 2;
 			break;
 		}
-		case MOVE_FORWARD: {
-			cout<<"MOVE_FORWARD"<<endl;
+		case ONCURVE_BEGIN: {
+			walkStep += 3;
+			place = ONCURVE_MIDDLE;
+			break;
+		}
+		case ONCURVE_MIDDLE: {
+			walkStep += 2;
+			place = ONCURVE_END:
+			break;
+		}
+		case ONCURVE_END: {
+			walkStep += 3;
+			place = ONLINE;
+			break;
+		}	 
+		}
+		status = LINE_DETECT;
+		cout << "STAYING" << endl;
+		break;
+	}
+	case MOVE_FORWARD: {
+		cout << "MOVE_FORWARD" << endl;
+		while (walkStep--)
 			Move_forward();
-			Move_forward();//Two steps at once-.-
-			status = STAY;
+		status = STAY;
+		break;
+	}
+	case LINE_DETECT: {
+		cout << "DETECTING_LINE" << endl;
+		double lineDetect = CapLine();
+		if ((lineDetect > -interimDegree && lineDetect < 0) || (lineDetect > 0 && lineDetect < interimDegree)) {
+			Angle = lineDetect;
+			status = MOVE_FORWARD;
 			break;
 		}
-		case LINE_DETECT: {
-			cout<<"DETECTING_LINE"<<endl;
-			double lineDetect = CapLine();
+		else if (lineDetect < -interimDegree && lineDetect >= -90) {
+			status = TURN_LEFT;
+			Angle = lineDetect;
+			break;
+		}
+		else if (lineDetect > interimDegree && lineDetect <= 90) {
+			status = TURN_RIGHT;
+			Angle = lineDetect;
+			break;
+		}
+		else if (lineDetect == 199909) {
+			status = MOVE_RIGHT;
+			break;
+		}
+		else if (lineDetect == 200012) {
+			status = MOVE_LEFT;
+			break;
+		}
+		else if (lineDetect == 200000) {
+			if (Angle < -interimDegree && Angle >= -90) {
+				status = TURN_LEFT;
+				//Angle = lineDetect;
+				break;
+			}
+			else if (Angle > interimDegree && Angle <= 90) {
+				status = TURN_RIGHT;
+				//Angle = lineDetect;
+				break;
+			}
+			else
+				status = MOVE_BACKWARD;
+			break;
+		}
+		else if (lineDeteck > 250000) {
+			switch (place) {
+				case ONLINE£º
+					place = ONCURVE_BEGIN;
+				default:
+			}
+			lineDetect -= 300000;
 			if ((lineDetect > -interimDegree && lineDetect < 0) || (lineDetect > 0 && lineDetect < interimDegree)) {
 				Angle = lineDetect;
 				status = MOVE_FORWARD;
@@ -65,64 +131,39 @@ void RoboTurtle::takeAction() {
 				Angle = lineDetect;
 				break;
 			}
-			else if (lineDetect == 199909) {
-				status = MOVE_RIGHT;
-				break;
-			}
-			else if (lineDetect == 200012) {
-				status = MOVE_LEFT;
-				break;
-			}
-			else if (lineDetect == 200000) {
-				if (Angle< -interimDegree && Angle >= -90) {
-					status = TURN_LEFT;
-					//Angle = lineDetect;
-					break;
-				}
-				else if (Angle > interimDegree && Angle <= 90) {
-					status = TURN_RIGHT;
-					//Angle = lineDetect;
-					break;
-				}
-				else
-					status = MOVE_BACKWARD;
-				break;
-			}
-			else if (lineDeteck > 250000) {
-				
-			}
-			break;
 		}
-		case MOVE_RIGHT: {
-			cout<<"MOVE_RIGHT"<<endl;
-			Move_right();
-			status = STAY;
-			break;
-		}
-		case MOVE_LEFT: {
-			cout<<"MOVE_LEFT"<<endl;
-			Move_left();
-			status = STAY;
-			break;
-		}
-		case TURN_RIGHT: {
-			cout<<"TURN_RIGHT "<<Angle<<" DEGREES"<<endl;
-			Turn_right(int(Angle));
-			status = STAY;
-			break;
-		}
-		case TURN_LEFT: {
-			cout<<"TURN_LEFT "<<Angle<<" DEGREES"<<endl;
-			Turn_left(int(-Angle));
-			status = STAY;
-			break;
-		}
-		case MOVE_BACKWARD: {
-			cout<<"MOVE_BACKWARD";
-			Move_backward();
-			status = STAY;
-			break;
-		}
+		break;
+	}
+	case MOVE_RIGHT: {
+		cout << "MOVE_RIGHT" << endl;
+		Move_right();
+		status = STAY;
+		break;
+	}
+	case MOVE_LEFT: {
+		cout << "MOVE_LEFT" << endl;
+		Move_left();
+		status = STAY;
+		break;
+	}
+	case TURN_RIGHT: {
+		cout << "TURN_RIGHT " << Angle << " DEGREES" << endl;
+		Turn_right(int(Angle));
+		status = STAY;
+		break;
+	}
+	case TURN_LEFT: {
+		cout << "TURN_LEFT " << Angle << " DEGREES" << endl;
+		Turn_left(int(-Angle));
+		status = STAY;
+		break;
+	}
+	case MOVE_BACKWARD: {
+		cout << "MOVE_BACKWARD";
+		Move_backward();
+		status = STAY;
+		break;
+	}
 	}
 }
 
